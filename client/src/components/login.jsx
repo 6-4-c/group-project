0496e862e7fc253ab536e8497234c7e7991189d4
Arlyn Viray <viray.arlyn@gmail.com>
2018-06-19 10:31:08 -0700

    fixed some issues

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import { Jumbotron , Grid, Row, Col, Image, Button } from 'react-bootstrap';
 import {Form, FormGroup, ControlLabel, Checkbox, FormControl } from 'react-bootstrap';
-import './login.css';
+// import './login.css';
 
 
 export default class Login extends Component {
