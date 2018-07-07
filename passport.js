cb4ac455eb2f8baeaa25e80204a8c8572efc8e2e
Robert Bender <Louis828@Roberts-MacBook-Pro-3.local>
2018-07-07 14:51:00 -0700

    added delete routes and updated to ES6

--- passport.js
+++ passport.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/user');
 
+const sessions = require('express-session');
+
+app.use(session ({secret: 'anything'}));
+
+app.use(passport.initialize());
+app.use(passport.sessions());
+app.use(express.sessions());
+
 passport.serializeUser(function(user, done) {
   done(null, {
       ID: user["ID"],
@@ -16,6 +24,7 @@ passport.deserializeUser(function(obj, done){
   done(null, obj);
 });
 
+//Local Strategy
 passport.use(new LocalStrategy(
   function(username, password, done) {
     User.findOne({ username: username }, function (err, user) {
@@ -26,3 +35,37 @@ passport.use(new LocalStrategy(
     });
   }
 ));
+
+// Google OAuth
+// var app = express();
+// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+//
+// passport.use(new GoogleStrategy({
+//         clientID: '668041538604-ippc98du4hrm5t1nngrgm4hb64gluan4.apps.googleusercontent.com',
+//         clientSecret: 'OkIOKGcdJjQTL7PTOiw5_KHL',
+//         callbackURL: '',
+//         function(req, accessToken, refreshToken, profile, done){
+//             done(null, profile);
+//         }
+// }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = passport;
---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+
+// Get Homepage
+router.get('/', ensureAuthenticated, function(req, res){
+	res.render('index');
+});
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		res.redirect('/users/login');
+	}
+}
+
+app.use(function(err, req, res, next) {
+  if (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
